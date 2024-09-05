2d9f038d20e9fda330834acc338e8a8c11d40335
unfflavio <164961481+unfflavio@users.noreply.github.com>
2024-09-05 01:13:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let anoAtual = 2024;
+const nomeCompleto = "flavio ferraz";
+let idade = 29;
+const cursoFaculdade = "circo";
+let cidadeAtual = "noruega";
+
+console.log(`olá eu sou o ${nomeCompleto}, tenho ${idade} anos, estou cursando ${cursoFaculdade}, moro na ${cidadeAtual}`);
+
+anoAtual = 2025;
+
+console.log(anoAtual)
